5aa5c81502b97b8ab03bed7b8c8724de34cfb8ed
Valentine Ayim <valentineayimodu@gmail.com>
2020-02-17 16:01:31 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+import {Switch, Route} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details'
+import Cart from './components/Cart';
+import Default from './components/Default';
+
+function App() {
+  return (
+    <React.Fragment>
+    <Navbar />
+    <Switch>
+    <Route exact path="/" component={ProductList}/>
+    <Route path="/details" component={Details}/>
+    <Route path="/cart" component={Cart} />
+    <Route component={Default}/>
+    
+    <Details />
+    <Cart />
+    <Default />
+    </Switch>
+
+    </React.Fragment>
+  );
+}
+
+export default App;
